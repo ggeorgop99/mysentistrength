28c3e44626fbd0a28eb70b0d60ae031386710a24
ggeorgop99 <ggeorgop1999@gmail.com>
2024-04-17 18:11:16 +0300

    [WIP] maybe fixed amazon_reviews22.py with request headers to avoid error 403

--- skroutz_scraping/amazon_reviews_scraping/amazon_reviews_scraping/settings.py
+++ skroutz_scraping/amazon_reviews_scraping/amazon_reviews_scraping/settings.py
@@ -12,20 +12,19 @@
 
 # [WIP] add the below to maybe fix 403 error
 
-HTTPERROR_ALLOWED_CODES  =[404]
-USER_AGENT = 'quotesbot (+http://www.yourdomain.com)'
+HTTPERROR_ALLOWED_CODES = [404]
+USER_AGENT = "quotesbot (+http://www.yourdomain.com)"
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
-
 # end of [WIP]
 
-BOT_NAME = 'skroutz_reviews_scraping'
+BOT_NAME = "skroutz_reviews_scraping"
 
-SPIDER_MODULES = ['amazon_reviews_scraping.spiders']
-NEWSPIDER_MODULE = 'amazon_reviews_scraping.spiders'
+SPIDER_MODULES = ["amazon_reviews_scraping.spiders"]
+NEWSPIDER_MODULE = "amazon_reviews_scraping.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'AUTH-SKG MASTER THESIS BOT'# (+http://www.google.com/adsbot.html)'#amazon_reviews_scraping (+http://www.yourdomain.com)'
+USER_AGENT = "AUTH-SKG MASTER THESIS BOT"  # (+http://www.google.com/adsbot.html)'#amazon_reviews_scraping (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -42,19 +41,17 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 1
 CONCURRENT_REQUESTS_PER_IP = 0
 
 
-
-
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -70,35 +67,35 @@ CONCURRENT_REQUESTS_PER_IP = 0
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-    #'amazon_reviews_scraping.pipelines.AmazonReviewsScrapingPipeline': 300,
+# ITEM_PIPELINES = {
+#'amazon_reviews_scraping.pipelines.AmazonReviewsScrapingPipeline': 300,
 #    'amazon_reviews_scraping.pipelines.DuplicatesPipeline': 300,
 
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
